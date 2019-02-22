d82964867361175f8fa614050fb266d2e326d066
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-02-22 20:03:01 +0900

    背景の描画

--- SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitileObj/TitileBack/TitleBack.cpp
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitileObj/TitileBack/TitleBack.cpp
@@ -2,10 +2,17 @@
 
 void TitleBack::Init()
 {
-	
+	m_rGameLib.CreateTex(_T("Back"), _T("Textures/Title/background_01_01.png"));
 }
 
 void TitleBack::Render()
 {
+	CustomVertex back[4];
+	ObjData data;
 
+	data.m_center = { m_WND_SIZE.m_x * 0.5f,m_WND_SIZE.m_y * 0.5f,0.0f };
+	data.m_halfScale = { m_WND_SIZE.m_x * 0.5f,m_WND_SIZE.m_y * 0.5f, 0.0f };
+
+	m_rGameLib.CreateRect(back, data);
+	m_rGameLib.Render(back, m_rGameLib.GetTex(_T("Back")));
 }
