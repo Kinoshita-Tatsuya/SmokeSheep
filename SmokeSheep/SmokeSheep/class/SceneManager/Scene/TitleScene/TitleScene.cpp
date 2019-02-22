f3d178a9a6f16c02a82abc220b204a9e6907a825
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-02-22 19:35:44 +0900

    TitleSceneクラスの枠組みの追加

---
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitleScene.cpp
@@ -0,0 +1,12 @@
+﻿#include "TitleScene.h"
+
+TitleScene::TitleScene()
+{
+
+}
+
+TitleScene::~TitleScene()
+
+{
+
+}
---
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitleScene.h
@@ -0,0 +1,13 @@
+﻿#ifndef TITLE_SCENE_H_
+#define TITLE_SCENE_H_
+
+#include "../Scene.h"
+
+class TitleScene :public Scene
+{
+public:
+	TitleScene();
+	~TitleScene();
+};
+
+#endif // !TITLE_SCENE_H_
