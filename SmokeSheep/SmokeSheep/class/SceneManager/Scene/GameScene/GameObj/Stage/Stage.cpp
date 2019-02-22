14fc92e616cf5d1d39db958feff77dee77821ae4
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-02-22 18:49:06 +0900

    Stageクラスの枠組み作成

---
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameObj/Stage/Stage.cpp
@@ -0,0 +1,25 @@
+﻿#include "Stage.h"
+
+#include<fstream>
+#include<sstream>
+#include<algorithm>
+
+void Stage::Init()
+{
+
+}
+
+void Stage::Update()
+{
+
+}
+
+void Stage::Render()
+{
+
+}
+
+void Stage::ReadStage(const char* pfileName)
+{
+
+}
---
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameObj/Stage/Stage.h
@@ -0,0 +1,26 @@
+﻿#ifndef STAGE_H_
+#define STAGE_H_
+
+#include <vector>
+#include <tchar.h>
+
+#include "../../../ObjManager/Obj/Obj.h"
+#include "STAGE_ID/STAGE_ID.h"
+
+class Stage :public Obj
+{
+public:
+	Stage();
+	~Stage();
+
+	void Init();
+	void Update();
+	void Render();
+
+private:
+	void ReadStage(const char* pfileName);
+
+	std::vector<std::vector<STAGE::ID>> m_stageIds;
+};
+
+#endif // !STAGE_H_
