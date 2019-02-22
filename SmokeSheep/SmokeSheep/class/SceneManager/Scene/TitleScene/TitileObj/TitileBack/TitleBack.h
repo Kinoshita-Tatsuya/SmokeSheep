b33b3a69ac6535c196cae5478799d063a2f9a6b5
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-02-22 19:36:15 +0900

    タイトル画面の背景描画クラスの作成

---
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitileObj/TitileBack/TitleBack.h
@@ -0,0 +1,25 @@
+﻿#ifndef TITLE_BACK_H_
+#define TITLE_BACK_H_
+
+#include "../../../ObjManager/Obj/Obj.h"
+
+class TitleBack :public Obj
+{
+public:
+	TitleBack() 
+	{
+		Init();
+	}
+	
+	~TitleBack()
+	{
+		m_rGameLib.ReleaseTex();
+	}
+
+	void Init();
+	void Update() {}
+	void Render();
+
+};
+
+#endif // !TITLE_BACK_H_
