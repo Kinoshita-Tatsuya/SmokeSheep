a5eab29364af941c0e108812fa95bcfe322f31ca
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-02-22 20:03:01 +0900

    背景の描画

--- SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitleScene.cpp
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitleScene.cpp
@@ -1,8 +1,9 @@
 ﻿#include "TitleScene.h"
+#include "TitileObj/TitileBack/TitleBack.h"
 
 TitleScene::TitleScene()
 {
-
+	m_objManager->Register(static_cast<Obj*>(new TitleBack()));
 }
 
 TitleScene::~TitleScene()
