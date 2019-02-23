31655ddab3393a2fbaf8e3192327572db2a75fda
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-02-23 13:27:13 +0900

    アニメーションの実装

---
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitileObj/TitleAnimations/TitleAnimations.h
@@ -0,0 +1,40 @@
+﻿#ifndef TITLE_ANIMATIONS_H_
+#define TITLE_ANIMATIONS_H_
+
+#include "../../../ObjManager/Obj/Obj.h"
+
+class TitleAnimations :public Obj
+{
+public:
+	TitleAnimations()
+	{
+		Init();
+	}
+
+	~TitleAnimations()
+	{
+
+	}
+
+	void Init();
+	void Update() {}
+	void Render();
+
+private:
+	enum CHARACTERS
+	{
+		TA_SHEEP,
+		TA_GRANDMA,
+		TA_BIRD,
+		TA_MAX
+	};
+
+	void SheepAnimation();
+	void GrandmaAnimation();
+	//void BirdAnimation();
+
+	int m_animationCounts[TA_MAX] = { 0,0,0 };		//アニメーションの画像カウント
+	ObjData m_animationDatas[TA_MAX];
+};
+
+#endif // !TITLE_ANIMATIONS_H_
