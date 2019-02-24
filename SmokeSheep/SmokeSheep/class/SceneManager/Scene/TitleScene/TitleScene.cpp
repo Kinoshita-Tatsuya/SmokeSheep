43b6da66c66f944426fa050f7722cf5ec0e99566
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-02-24 19:17:03 +0900

    オブジェクトマネージャーに登録

--- SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitleScene.cpp
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/TitleScene/TitleScene.cpp
@@ -2,12 +2,14 @@
 #include "TitileObj/TitileBack/TitleBack.h"
 #include "TitileObj/TitleLogo/TitleLogo.h"
 #include "TitileObj/TitleAnimations/TitleAnimations.h"
+#include "TitileObj/TitleSelectMenu/TitleSelectMenu.h"
 
 TitleScene::TitleScene()
 {
 	m_objManager->Register(static_cast<Obj*>(new TitleBack()));
 	m_objManager->Register(static_cast<Obj*>(new TitleLogo()));
 	m_objManager->Register(static_cast<Obj*>(new TitleAnimations()));
+	m_objManager->Register(static_cast<Obj*>(new TitleSelectMenu()));
 }
 
 TitleScene::~TitleScene()
