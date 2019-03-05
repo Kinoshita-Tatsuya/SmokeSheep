2dfee9caa489474a76e1adc25b30aec9a2737560
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-05 18:17:29 +0900

    ブロック管理クラスの作成

--- SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameObj/Stage/BlockManager/BlockManager.cpp
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameObj/Stage/BlockManager/BlockManager.cpp
@@ -1,1 +1,78 @@
-﻿
+﻿#include "BlockManager.h"
+
+#include "BaseBlock/GrassBlock/GrassBlock.h"
+
+void BlockManager::Init()
+{
+	for (int i = 0;i < STAGE::BLOCK_MAX;++i)
+	{
+		m_rgameLib.CreateTex(m_ptextureKey[i], m_ptexturePath[i]);
+	}
+}
+
+void BlockManager::Update()
+{
+
+}
+
+void BlockManager::Render()
+{
+
+}
+
+void BlockManager::Create(STAGE::BLOCK_ID blockId, int arrayX, int arrayY)
+{
+	BlockData data;
+	BaseBlock* baseBlock = nullptr;
+
+	data.m_arrayXNum = arrayX;
+	data.m_arrayYNum = arrayY;
+	data.m_ptextureKey = m_ptextureKey[blockId];
+
+	switch (blockId)
+	{
+	case STAGE::NONE:
+
+		break;
+
+	case STAGE::GROUND:
+
+		break;
+
+	case STAGE::GRASS:
+		baseBlock = new GrassBlock(data);
+
+		break;
+
+	case STAGE::GROUND_NIGHT:
+		
+		break;
+
+	case STAGE::GRASS_NIGHT:
+		
+		break;
+
+	case STAGE::ICE:
+		
+		break;
+
+	case STAGE::SNOW:
+		
+		break;
+
+	case STAGE::IVY:
+	
+		break;
+
+	case STAGE::DIE:
+		
+		break;
+
+	case STAGE::GOAR:
+		
+		break;
+
+	default:
+		break;
+	}
+}
