2b4c69299e3b4fbe03bd94fc7dcfd818d12b38c9
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-06 19:59:33 +0900

    ステージクラスにブロック管理クラスを内包させる

--- SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameObj/Stage/BlockManager/BaseBlock/IvyBlock/IvyBlock.h
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameObj/Stage/BlockManager/BaseBlock/IvyBlock/IvyBlock.h
@@ -1,1 +1,21 @@
-﻿#pragma once
+﻿#ifndef IVY_BLOCK_H_
+#define IVY_BLOCK_H_
+
+#include "../BaseBlock.h"
+
+class IvyBlock :public BaseBlock
+{
+public:
+	IvyBlock(BlockData blockData) :BaseBlock(blockData)
+	{
+
+	}
+
+	~IvyBlock()
+	{
+
+	}
+
+};
+
+#endif // ! IVY_BLOCK_H_
--- SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameObj/Stage/Stage.h
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameObj/Stage/Stage.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "../../../ObjManager/Obj/Obj.h"
+#include "BlockManager/BlockManager.h"
 
 class Stage :public Obj
 {
@@ -24,8 +25,11 @@ public:
 
 private:
 	void ReadStage(const char* pfileName);
+	void CreateBlock(int stageId, int arrayX, int arrayY);
 
 	std::vector<std::vector<int>> m_stageIds;
+
+	BlockManager m_blockManager;
 
 	float m_xScroll = 0.f;
 	float m_yScroll = 0.f;
