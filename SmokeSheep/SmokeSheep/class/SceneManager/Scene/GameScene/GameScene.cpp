75ee9632bddca0cfd7c354fb81ef244e49fe0152
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-13 17:10:04 +0900

    オブジェクトマネージャーにプレイヤークラスを登録

--- SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameScene.cpp
+++ SmokeSheep/SmokeSheep/class/SceneManager/Scene/GameScene/GameScene.cpp
@@ -1,9 +1,11 @@
 ﻿#include "GameScene.h"
 #include "GameObj/Stage/Stage.h"
+#include "GameObj/Player/Player.h"
 
 GameScene::GameScene()
 {
 	m_pobjManager->Register(static_cast<Obj*>(new Stage()));
+	m_pobjManager->Register(static_cast<Obj*>(new Player()));
 }
 
 GameScene::~GameScene()
